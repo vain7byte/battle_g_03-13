c2f78921d18caaca5ed00d2b5faae1552128638c
vain7byte <vainstream@gmail.com>
2013-03-07 12:17:27 +0100

    ex 10 treppe

---
+++ ex10.py
@@ -0,0 +1,27 @@
+﻿stairs = "_______"
+stab = "|"
+end = 8
+
+fat_cat = """
+I'll do a stairs:
+\t %s
+\t\t %s
+\t\t\t %s\n\t\t\t\t %s \n|
+""" % (stairs, stairs, stairs, stairs)
+
+for i in range(0, end):
+    tab = "\t" * i
+    if i == 0:
+        print stairs + "_"
+    if 0 < i < end - 1:
+        print tab + stab + stairs
+    if i == end - 1:
+        print tab + stab
+        
+#tabby_cat = "\tI'm tabbed in."
+# persian_cat = "I'm split\non a line."
+# backslash_cat = "I'm \\ a \\ cat."
+# print tabby_cat
+# print persian_cat
+# print backslash_cat
+# print fat_cat
---
+++ ex7.py
@@ -0,0 +1,24 @@
+﻿# -*- coding: utf-8 -*-
+
+furzstaerke = 100 * 1#000
+luftdichte = round(20.64)
+letter = "a"
+
+# print u"G" + letter * furzstaerke + "s"
+print u"G" + "a" * furzstaerke + "s"
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
